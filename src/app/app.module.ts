bc2651d494fc8ebb44b0835cfb8ec7e450abda81
biancahpp <bianca_procopio@hotmail.com>
2021-07-07 18:52:06 -0600

    basic design done, working on api call next

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+// import { MatIconModule } from '@angular/material/icon';
+
+// const MaterialComponents = [MatIconModule];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, DashboardComponent, MovieListComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
